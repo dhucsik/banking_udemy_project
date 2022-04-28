95c9348e2f241dd374f95e0be5d4d7fc53c970e5
dias <diasgalikhanov@gmail.com>
2022-04-28 19:38:56 +0600

    post method and import gorilla/mux

--- app/app.go
+++ app/app.go
@@ -3,15 +3,17 @@ package app
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
-	mux := http.NewServeMux()
+	router := mux.NewRouter()
 
 	//define routes
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet)
+	router.HandleFunc("/customers", getAllCustomers)
 
 	//starting server
-	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dhucsik/banking_udemy_project
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
