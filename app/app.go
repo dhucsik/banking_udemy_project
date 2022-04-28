7c6bd81bc3e496a48304307c74e96189661e439a
dias <diasgalikhanov@gmail.com>
2022-04-28 19:44:50 +0600

    d

--- app/app.go
+++ app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -11,9 +12,21 @@ func Start() {
 	router := mux.NewRouter()
 
 	//define routes
-	router.HandleFunc("/greet", greet)
-	router.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	//starting server
 	log.Fatal(http.ListenAndServe("localhost:8000", router))
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post request received")
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, vars["customer_id"])
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/dhucsik/banking_udemy_project
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/gorilla/mux v1.8.0
